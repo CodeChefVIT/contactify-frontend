63105ce60dbc2efe39e5d587f2e8d33106743fbd
Aaditya Mahanta <aadityamahanta07@gmail.com>
2024-09-06 00:57:59 +0530

    more seo

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,8 +5,9 @@ import Head from "next/head";
 import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
-  title: "Contactify",
-  description: "Excel to vCard converter",
+  title: "Contactify by CodeChef-VIT",
+  description:
+    "Contactify offers a seamless solution for users to upload contact information in various formats (CSV, XLSX, or XLS). Upon submission, the backend processes this data, extracting details such as names, phone numbers, emails, and genders. It compiles this information into a unified contact file (VCF file) compatible with Android and iOS devices, simplifying contact management.",
   icons: [
     { rel: "icon", url: "/favicon.png" },
     { rel: "icon", sizes: "32x32", url: "/favicon.png" },
@@ -17,15 +18,40 @@ export const metadata: Metadata = {
   openGraph: {
     type: "website",
     url: "https://contactify.codechefvit.com",
-    title: "Contactify",
-    description: "Excel to vCard converter",
+    title: "Contactify by CodeChef-VIT",
+    description:
+      "Contactify offers a seamless solution for users to upload contact information in various formats (CSV, XLSX, or XLS). Upon submission, the backend processes this data, extracting details such as names, phone numbers, emails, and genders. It compiles this information into a unified contact file (VCF file) compatible with Android and iOS devices, simplifying contact management.",
     siteName: "Contactify",
     images: [
       {
-        url: "https://contactify.codechefvit.com/favicon.png",
-        type: "image/png",
+        url: "https://contactify.codechefvit.com/favicon-black.svg",
+        type: "image/svg",
       },
     ],
+  },
+  keywords: [
+    "contactify",
+    "codechef",
+    "vit",
+    "contact",
+    "contacts",
+    "csv",
+    "xlsx",
+    "xls",
+    "vcf",
+    "excel",
+    "json",
+    "contact card",
+    "vcf",
+    "vcard",
+  ],
+  authors: [{ name: "Aaditya Mahanta" }],
+  twitter: {
+    card: "summary_large_image",
+    site: "@contactify",
+    title: "Contactify by CodeChef-VIT",
+    creator: "Aaditya Mahanta (CodeChef-VIT)",
+    images: "https://contactify.codechefvit.com/favicon-black.svg",
   },
   metadataBase: new URL("https://contactify.codechefvit.com"),
 };
